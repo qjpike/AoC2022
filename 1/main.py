7b99128dd24add9ff2f48b111a2946ec4a4914cf
Quinten Pike <qpike@archer.com>
2022-11-30 21:09:55 -0800

    day 1

---
+++ 1/main.py
@@ -0,0 +1,27 @@
+
+def get_input():
+    inp = []
+    with open("input.txt") as f:
+        for i in f.readlines():
+            if i == '\n':
+                inp.append(0)
+            else:
+                inp.append(int(i))
+            # inp.append(i.strip())
+            # inp.append(i.split())
+
+    return inp
+
+dat = get_input()
+counts = []
+curr_sum = 0
+for i in dat:
+    if i == 0:
+        counts.append(curr_sum)
+        curr_sum = 0
+    else:
+        curr_sum += i
+
+counts.sort()
+print("1:", counts[-1])
+print("2:", sum(counts[-3:]))
